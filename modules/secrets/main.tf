c766d1c9fa11148894c06828ca6f08c6a8726490
Heath Smith <heathsmithaws2025@gmail.com>
2026-08-10 19:10:55 -0400

    security: generate database password with Secrets Manager

--- modules/secrets/main.tf
+++ modules/secrets/main.tf
@@ -1,3 +1,9 @@
+ephemeral "aws_secretsmanager_random_password" "db" {
+  password_length            = 32
+  exclude_characters         = "\"'`\\/@"
+  require_each_included_type = true
+}
+
 resource "aws_secretsmanager_secret" "db" {
   name                    = "${var.project}-${var.environment}-db-credentials"
   recovery_window_in_days = var.recovery_window_in_days
@@ -11,8 +17,10 @@ resource "aws_secretsmanager_secret" "db" {
 resource "aws_secretsmanager_secret_version" "db" {
   secret_id = aws_secretsmanager_secret.db.id
 
-  secret_string = jsonencode({
+  secret_string_wo = jsonencode({
     username = "admin"
-    password = "ChangeMe123!"
+    password = ephemeral.aws_secretsmanager_random_password.db.random_password
   })
+
+  secret_string_wo_version = 1
 }
