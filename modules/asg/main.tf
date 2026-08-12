be74e6a4e6e2a87dad68d585eea12888c95b0515
Heath Smith <heathsmithaws2025@gmail.com>
2026-08-12 09:01:42 -0400

    feat: add portfolio landing page to application tier

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -34,12 +34,12 @@ resource "aws_launch_template" "this" {
     name = aws_iam_instance_profile.this.name
   }
 
-  user_data = base64encode(<<-EOF
-        #!/bin/bash
-        systemctl start httpd
-        echo "Hello from EC2" > /var/www/html/index.html
-        EOF
-  )
+  user_data = base64encode(templatefile(
+    "${path.module}/templates/userdata.sh.tftpl",
+    {
+      environment = var.environment
+    }
+  ))
 
   tag_specifications {
     resource_type = "instance"
